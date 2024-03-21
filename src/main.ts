258a064644dbd3deb0fcbc4257de34dd6543a952
yuva <yuva.prasath@flyerssoft.com>
2024-03-21 23:19:10 +0530

    port

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT;
   const config = new DocumentBuilder()
     .setTitle('Project management')
     .setDescription('API for project management')
@@ -20,6 +21,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   app.enableCors();
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000);
+  await app.listen(port);
 }
 bootstrap();
